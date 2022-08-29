85de8ff7ce9198b839adfc8cd9910ee2ed1e7d60
James Gentes <jgentes@gmail.com>
2022-08-28 19:33:54 -0700

    test header working

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 //import AppLayout from './layout/layout'
-import { EuiProvider, EuiIcon, EuiCode, EuiText, useEuiTheme } from '@elastic/eui'
+import {
+  EuiProvider,
+  EuiIcon,
+  EuiCode,
+  EuiText,
+  useEuiTheme,
+} from '@elastic/eui'
 import { db } from './db'
 import { useLiveQuery } from 'dexie-react-hooks'
-import '@elastic/eui/dist/eui_theme_dark.css'; // these will go away once eui transitions entirely to emotion rather than sass
-import '@elastic/eui/dist/eui_theme_light.css'; // https://github.com/elastic/eui/blob/main/wiki/consuming.md
+import { AppLayout } from './layout/appLayout'
 
-import { Test} from './test'
-
+// styles
+import './styles/icons'
+import '@elastic/eui/dist/eui_theme_dark.css' // these will go away once eui transitions entirely to emotion rather than sass
+import '@elastic/eui/dist/eui_theme_light.css' // https://github.com/elastic/eui/blob/main/wiki/consuming.md
 
 const App = () => {
-  const colorMode =
-    useLiveQuery((): Promise<boolean> => db.appState.get('darkMode')) ? 'dark' : 'light'
+  const colorMode = useLiveQuery(
+    (): Promise<boolean> => db.appState.get('darkMode')
+  )
+    ? 'dark'
+    : 'light'
 
   return (
-    <EuiProvider colorMode={'light'}>
-      <Test/>
-  </EuiProvider>
+    <EuiProvider colorMode={colorMode}>
+      <AppLayout />
+    </EuiProvider>
   )
 }
-
 
 export default App
--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,21 +1,27 @@
-import { ICON_TYPES } from '@elastic/eui';
-import { ValuesType } from 'utility-types';
+import { ICON_TYPES } from '@elastic/eui'
+import { ValuesType } from 'utility-types'
 
-import { appendIconComponentCache } from '@elastic/eui/es/components/icon/icon';
+import { appendIconComponentCache } from '@elastic/eui/es/components/icon/icon'
 
-import { icon as magnifyWithMinus } from '@elastic/eui/es/components/icon/assets/magnifyWithMinus';
-import { icon as minus } from '@elastic/eui/es/components/icon/assets/minus';
-import { icon as magnifyWithPlus } from '@elastic/eui/es/components/icon/assets/magnifyWithPlus';
-import { icon as plus } from '@elastic/eui/es/components/icon/assets/plus';
+import { icon as magnifyWithMinus } from '@elastic/eui/es/components/icon/assets/magnifyWithMinus'
+import { icon as minus } from '@elastic/eui/es/components/icon/assets/minus'
+import { icon as magnifyWithPlus } from '@elastic/eui/es/components/icon/assets/magnifyWithPlus'
+import { icon as plus } from '@elastic/eui/es/components/icon/assets/plus'
+import { icon as search } from '@elastic/eui/es/components/icon/assets/search'
+import { icon as arrowDown } from '@elastic/eui/es/components/icon/assets/arrow_down'
+import { icon as dot } from '@elastic/eui/es/components/icon/assets/dot'
 
-type IconComponentNameType = ValuesType<typeof ICON_TYPES>;
-type IconComponentCacheType = Partial<Record<IconComponentNameType, unknown>>;
+type IconComponentNameType = ValuesType<typeof ICON_TYPES>
+type IconComponentCacheType = Partial<Record<IconComponentNameType, unknown>>
 
 const cachedIcons: IconComponentCacheType = {
   magnifyWithMinus,
   magnifyWithPlus,
   minus,
-  plus,  
-};
+  plus,
+  search,
+  arrowDown,
+  dot,
+}
 
-appendIconComponentCache(cachedIcons);
+appendIconComponentCache(cachedIcons)
