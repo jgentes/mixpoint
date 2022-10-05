b5965b2135289319162bd8f668375dfdd2abe2aa
James Gentes <jgentes@gmail.com>
2022-10-05 12:31:19 -0700

    added remove tracks button

--- app/components/tracks/tableHeader.tsx
+++ app/components/tracks/tableHeader.tsx
@@ -1,28 +1,38 @@
-import { Add, Delete, PriorityHigh, SearchRounded } from '@mui/icons-material'
 import {
+  Add,
+  Delete,
+  PriorityHigh,
+  SearchRounded,
+  WarningRounded,
+} from '@mui/icons-material'
+import {
+  Box,
   Button,
   Card,
   Checkbox,
   IconButton,
+  Modal,
+  ModalDialog,
   TextField,
   Typography,
 } from '@mui/joy'
 import {
+  Divider,
   TableCell,
   TableHead,
   TableRow,
   TableSortLabel,
   Toolbar,
-  Tooltip,
 } from '@mui/material'
 import { alpha } from '@mui/material/styles'
 import { visuallyHidden } from '@mui/utils'
 import { superstate } from '@superstate/core'
 import { useSuperState } from '@superstate/react'
-import { ChangeEvent, MouseEvent, useMemo } from 'react'
+import { ChangeEvent, MouseEvent, useMemo, useState } from 'react'
 import { dirtyTrackState } from '~/api/audio'
-import { AppState, Track } from '~/api/db'
+import { AppState, removeTrack, Track } from '~/api/db'
 import { browseFile } from '~/api/fileHandlers'
+import { selectedState } from '~/routes/__boundary/tracks'
 import { createColumnDefinitions } from './tableColumns'
 
 // Broadcast search query
@@ -32,6 +42,8 @@ const searchState = superstate<string | number>('')
 const EnhancedTableToolbar = (props: { numSelected: number }) => {
   useSuperState(searchState)
   useSuperState(dirtyTrackState)
+
+  const [openRemoveModal, setOpenRemoveModal] = useState(false)
 
   const { numSelected } = props
 
@@ -106,11 +118,68 @@ const EnhancedTableToolbar = (props: { numSelected: number }) => {
           </div>
         </>
       ) : (
-        <Tooltip title="Delete">
-          <IconButton>
+        <>
+          <IconButton
+            variant="plain"
+            title="Remove tracks"
+            size="sm"
+            color="neutral"
+            onClick={() => setOpenRemoveModal(true)}
+          >
             <Delete />
           </IconButton>
-        </Tooltip>
+          <Modal
+            aria-labelledby="alert-dialog-modal-title"
+            aria-describedby="alert-dialog-modal-description"
+            open={openRemoveModal}
+            sx={{ alignItems: 'normal' }}
+            onClose={() => setOpenRemoveModal(false)}
+          >
+            <ModalDialog variant="outlined" role="alertdialog">
+              <Typography
+                id="alert-dialog-modal-title"
+                component="h2"
+                level="inherit"
+                fontSize="1.25em"
+                mb="0.25em"
+                startDecorator={<WarningRounded />}
+              >
+                Are you sure?
+              </Typography>
+              <Divider sx={{ my: 2 }} />
+              <Typography
+                id="alert-dialog-modal-description"
+                textColor="text.tertiary"
+                mb={3}
+              >
+                Removing them here <b>will not</b> delete them from your
+                computer.
+              </Typography>
+              <Box sx={{ display: 'flex', gap: 1, justifyContent: 'flex-end' }}>
+                <Button
+                  variant="plain"
+                  color="neutral"
+                  onClick={() => setOpenRemoveModal(false)}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  variant="solid"
+                  color="danger"
+                  onClick={async () => {
+                    setOpenRemoveModal(false)
+                    for (const track of selectedState.now()) {
+                      await removeTrack(track)
+                    }
+                    selectedState.set([])
+                  }}
+                >
+                  Remove {numSelected} track{numSelected > 1 ? 's' : ''}
+                </Button>
+              </Box>
+            </ModalDialog>
+          </Modal>
+        </>
       )}
     </Toolbar>
   )
