51eb3badc2624291468f4cb200478b6bc5488edb
James Gentes <jgentes@gmail.com>
2022-11-01 07:06:40 -0700

    fallback to defaults

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
   serverBuildTarget: 'cloudflare-pages',
-  publicPath: '/build/',
-  serverBuildDirectory: 'build',
-  browserBuildDirectory: 'public/build',
-  server: './server.js',
+  server: 'server.js',
 }
