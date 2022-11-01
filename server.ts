e65a1e343c678e8b10572e7dd2dc2289c0079e9a
James Gentes <jgentes@gmail.com>
2022-11-01 09:30:18 -0700

    vercel

--- server.ts
+++ server.ts
@@ -1,12 +1,4 @@
-import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages'
-import * as build from '@remix-run/dev/server-build'
-
-const handleRequest = createPagesFunctionHandler({
-  build,
-  mode: process.env.NODE_ENV,
-  getLoadContext: context => context.env,
+const { createRequestHandler } = require('@remix-run/vercel')
+module.exports = createRequestHandler({
+  build: require('./build'),
 })
-
-export function onRequest(context) {
-  return handleRequest(context)
-}
