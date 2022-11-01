d5d4f8d8d2afbfb7c6d62378eabc21742bed1103
James Gentes <jgentes@gmail.com>
2022-11-01 07:26:36 -0700

    fixed breaking dependency

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  appDirectory: 'app',
-  serverBuildTarget: 'cloudflare-pages',
-  server: 'server.js',
-}
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? {
+        serverBuildTarget: 'cloudflare-pages',
+        server: 'server.js',
+      }
+    : {}
