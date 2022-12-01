ed721665c7ac16fb906b9e01f658d5d1c0447168
James Gentes <jgentes@gmail.com>
2022-12-01 07:20:11 -0800

    added volumemeter.tsx

---
+++ app/components/mixes/VolumeMeter.tsx
@@ -0,0 +1,46 @@
+import { LinearProgress } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { audioEvent } from '~/api/audioEvents'
+import { Track } from '~/api/dbHandlers'
+
+const VolumeMeter = ({ trackId }: { trackId: Track['id'] }) => {
+  if (!trackId) return null
+
+  const [volume, setVolume] = useState(0)
+
+  const updateVolume = ({
+    event,
+    args,
+  }: {
+    event: string
+    args: { volume: number }
+  }) => {
+    if (event === 'volumeMeter') {
+      setVolume(args.volume)
+    }
+  }
+
+  useEffect(() => {
+    audioEvent.on(trackId, updateVolume)
+    return audioEvent.off(trackId, updateVolume)
+  })
+
+  return (
+    <LinearProgress
+      id={`volume-container_${trackId}`}
+      variant="determinate"
+      value={100 - volume}
+      sx={{
+        margin: '-1px 5px 0',
+        zIndex: 0,
+        transform: 'rotate(180deg)',
+        background: `linear-gradient(to left, rgba(30, 150, 0, .75) 30%, rgba(255, 242, 0, .75) 70%, rgba(255, 0, 0, .75) 99%)`,
+        '> span': {
+          backgroundColor: '#0a1929bd',
+        },
+      }}
+    />
+  )
+}
+
+export { VolumeMeter as default }
