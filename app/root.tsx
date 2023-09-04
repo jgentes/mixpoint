b8c34b1f14c0bc28011963b0a492ecbfa0c2bfc3
James Gentes <jgentes@gmail.com>
2023-09-04 13:44:45 -0700

    changed default mode to dark

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,18 @@
 // this file establishes the root component that renders all subsequent / child routes
 // it also injects top level styling, HTML meta tags, links, and javascript for browser rendering
-import PublicSansFont from '@fontsource/public-sans/latin.css'
-import { CssVarsProvider } from '@mui/joy/styles'
-import { CssBaseline } from '@mui/material'
-import { LinksFunction, V2_MetaFunction } from '@remix-run/cloudflare'
-import { Links, LiveReload, Meta, Outlet, Scripts } from '@remix-run/react'
-import { SnackbarProvider } from 'notistack'
-import { useEffect, useState } from 'react'
-import { createHead } from 'remix-island'
-import { ClientOnly } from 'remix-utils'
-import ConfirmModal from '~/components/ConfirmModal'
-import InitialLoader from '~/components/InitialLoader'
-import styles from '~/root.css'
-import { theme } from '~/theme'
+import PublicSansFont from "@fontsource/public-sans/latin.css";
+import { CssVarsProvider } from "@mui/joy/styles";
+import { CssBaseline } from "@mui/material";
+import { LinksFunction, V2_MetaFunction } from "@remix-run/cloudflare";
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
+import { SnackbarProvider } from "notistack";
+import { useEffect, useState } from "react";
+import { createHead } from "remix-island";
+import { ClientOnly } from "remix-utils";
+import ConfirmModal from "~/components/ConfirmModal";
+import InitialLoader from "~/components/InitialLoader";
+import styles from "~/root.css";
+import { theme } from "~/theme";
 
 // this is needed to address React 18.2 hydration issues
 // TODO - remove this once React 18.3 is released
@@ -21,24 +21,24 @@ export const Head = createHead(() => (
 		<Meta />
 		<Links />
 	</>
-))
+));
 
 const meta: V2_MetaFunction = () => [
-	{ title: 'Mixpoint' },
-	{ description: 'Mixpoint is multi-track audio mixing app for the browser' },
-	{ viewport: 'width=device-width, initial-scale=1' }
-]
+	{ title: "Mixpoint" },
+	{ description: "Mixpoint is multi-track audio mixing app for the browser" },
+	{ viewport: "width=device-width, initial-scale=1" },
+];
 
 const links: LinksFunction = () => [
 	{
-		rel: 'icon',
-		type: 'image/png',
-		href: '/media/innerjoin32.png',
-		sizes: '32x32'
+		rel: "icon",
+		type: "image/png",
+		href: "/media/innerjoin32.png",
+		sizes: "32x32",
 	},
-	{ rel: 'stylesheet', href: PublicSansFont },
-	{ rel: 'stylesheet', href: styles }
-]
+	{ rel: "stylesheet", href: PublicSansFont },
+	{ rel: "stylesheet", href: styles },
+];
 
 const HtmlDoc = ({ children }: { children: React.ReactNode }) => {
 	return (
@@ -46,23 +46,23 @@ const HtmlDoc = ({ children }: { children: React.ReactNode }) => {
 			{children}
 			<LiveReload />
 		</>
-	)
-}
+	);
+};
 
 const ThemeLoader = ({ noSplash }: { noSplash?: boolean }) => {
-	const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(true);
 
 	// InitialLoader is used to hide the flash of unstyled content
 	useEffect(() => {
 		const timer = setTimeout(() => {
-			setLoading(false)
-		}, 500)
-		return () => clearTimeout(timer)
-	}, [])
+			setLoading(false);
+		}, 500);
+		return () => clearTimeout(timer);
+	}, []);
 
 	return (
 		<SnackbarProvider preventDuplicate maxSnack={3}>
-			<CssVarsProvider theme={theme} defaultMode={'light'}>
+			<CssVarsProvider theme={theme} defaultMode={"dark"}>
 				{/* CSS Baseline is used to inject global styles */}
 				<CssBaseline />
 				{loading && !noSplash ? (
@@ -75,14 +75,14 @@ const ThemeLoader = ({ noSplash }: { noSplash?: boolean }) => {
 				)}
 			</CssVarsProvider>
 		</SnackbarProvider>
-	)
-}
+	);
+};
 
 const App = () => (
 	<HtmlDoc>
 		<ThemeLoader />
 		<Scripts />
 	</HtmlDoc>
-)
+);
 
-export { ThemeLoader, App as default, links, meta }
+export { ThemeLoader, App as default, links, meta };
