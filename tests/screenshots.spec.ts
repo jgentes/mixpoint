3b5e28a4d5e0499abb939c78ad7a56bd27d8c66d
James Gentes <jgentes@gmail.com>
2023-12-11 20:41:22 -0800

    screenshot working

---
+++ tests/screenshots.spec.ts
@@ -0,0 +1,14 @@
+import { argosScreenshot } from '@argos-ci/playwright'
+import { test } from '@playwright/test'
+
+const baseUrl = 'http://localhost:8788'
+const pages = [{ name: 'homepage', path: '/' }] // add more pages here as needed
+
+test('screenshot pages', async ({ page }, workerInfo) => {
+	for (const { name, path } of pages) {
+		const browserName = workerInfo.project.name
+		await page.goto(`${baseUrl}${path}`)
+		await page.waitForTimeout(3000) // wait for loader screen
+		await argosScreenshot(page, `${name}-${browserName}`)
+	}
+})
