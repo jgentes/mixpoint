0664f0708d61fcdb067532ff561fa5296367ad35
James Gentes <jgentes@gmail.com>
2023-12-12 21:04:51 -0800

    even longer wait

--- tests/screenshots.spec.ts
+++ tests/screenshots.spec.ts
@@ -8,7 +8,7 @@ test('screenshot pages', async ({ page }, workerInfo) => {
 	for (const { name, path } of pages) {
 		const browserName = workerInfo.project.name
 		await page.goto(`${baseUrl}${path}`)
-		await page.waitForTimeout(2500) // wait for loader screen
+		await page.waitForTimeout(3000) // wait for loader screen
 		await argosScreenshot(page, `${name}-${browserName}`)
 	}
 })
